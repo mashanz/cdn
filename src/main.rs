7f1f6f0b29a455ceddfe889b9e494dcd7757183c
mashanz <tech@mashanz.com>
2024-10-15 02:56:46 +0000

    add: logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use actix_files as fs;
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
-        App::new().service(
-            fs::Files::new("/", ".")
+        App::new()
+            .wrap(Logger::new("%a %r %b %{Referer}i %{User-Agent}i %T"))
+            .service(fs::Files::new("/", ".")
                 .show_files_listing()
                 .use_last_modified(true),
         )

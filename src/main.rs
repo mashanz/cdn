0d8c9dd41965ec040011b9a2b2e90d46cc434ea3
Mashanz <mangatkk@gmail.com>
2024-10-17 14:37:53 +0700

    fix: cors

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use actix_files as fs;
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
 use env_logger::Env;
+use actix_cors::Cors;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-
     HttpServer::new(|| {
+        let cors = Cors::permissive();
         App::new()
             .wrap(Logger::new("%a %r %b %{Referer}i %{User-Agent}i %T"))
+            .wrap(cors)
             .service(fs::Files::new("/", ".")
                 .show_files_listing()
                 .use_last_modified(true),
